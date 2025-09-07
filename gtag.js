aaaf9f74b6b87aa6210a81dd87e7ca6a329dbf9c
Denker592 <denker592@gmail.com>
2025-09-07 18:31:33 +0300

    gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B6MKQ6BLDQ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B6MKQ6BLDQ');
+</script>
